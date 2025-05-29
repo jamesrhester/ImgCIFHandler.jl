c1c19fd46696fd2cded97bad35a7e2976efbd9e6
James Hester <jamesrhester@gmail.com>
2025-05-29 18:35:30 +1000

    Debuggin testing server

--- test/server_tests.jl
+++ test/server_tests.jl
@@ -4,7 +4,7 @@ using LiveServer
 
 fire_up_server() = begin
     serve_dir = joinpath(@__DIR__, "testfiles/test_cbf_unzipped")
-    @async LiveServer.serve(dir = serve_dir, port=8001, verbose=true)
+    @async LiveServer.serve(dir = serve_dir, port=8001, verbose=true, debug=true)
 end
 
 prepare_cf() = begin
@@ -14,7 +14,7 @@ prepare_cf() = begin
 end
 
 fire_up_server()
-sleep(2)   #give server time to start
+sleep(5)   #give server time to start
 
 @testset "http + TBZ" begin
     cf, la = prepare_cf()
